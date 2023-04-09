a0d560ea5259a01252760e8f0e64f28198ba3b2a
yuliyaparamonava <yuliyaparamonava@localhost>
2023-04-09 19:27:04 -0400

    added getAddress()

--- src/day2hwproject4/Class1.java
+++ src/day2hwproject4/Class1.java
@@ -6,4 +6,9 @@ public static void main(String[] args) {
 	System.out.println("Still practicing stashing and pushing...");
 	
 }
+
+public void getAddress() {
+	System.out.println("My address is Alexandria, VA");
+}
+
 }
